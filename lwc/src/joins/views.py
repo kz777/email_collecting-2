da4409f2d5c773dd6d33195d5f1535e39ba02b4b
ASLAN-USSAYEV <usprof777@gmail.com>
2015-04-08 10:50:12 -0500

    creating forms for the app

--- lwc/src/joins/views.py
+++ lwc/src/joins/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .forms import EmailForm, JoinForm
+from .models import Join
+
 def home(request):
-	context = {}
+
+
+	# form = EmailForm(request.POST or None)
+	# if form.is_valid():
+	# 	email=form.cleaned_data['email']
+	# 	new_join, created = Join.objects.get_or_create(email= email)
+	# 	print new_join, created
+	# 	print new_join.timestamp
+
+	form = JoinForm(request.POST or None)
+	if form.is_valid():
+		new_join = form.save(commit=False)
+		email=form.cleaned_data['email']
+	 	new_join_old, created = Join.objects.get_or_create(email= email)
+		#new_join.save()
+
+	context = {"form": form}
 	template = "home.html"
 	return render(request,template, context)
--- lwc/src/lwc/views.py
+++ lwc/src/lwc/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
 
-def home(request):
-	context = {}
-	template = "home.html"
-	return render(request,template, context) 
+# def home(request):
+# 	context = {}
+# 	template = "home.html"
+# 	return render(request,template, context) 
+
+
+ # def home(request):
+	# context = {}
+	# template = "home.html"
+	# return render(request,template, context) 
+
+
+ 
