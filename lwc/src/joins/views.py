1ecc21b235127f56d0c0255fe2842b5672401790
ASLAN-USSAYEV <usprof777@gmail.com>
2015-04-08 20:23:18 -0500

    Creating a Social Sharing Page to Share

--- lwc/src/joins/views.py
+++ lwc/src/joins/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 
 # Create your views here.
 from .forms import EmailForm, JoinForm
@@ -27,18 +27,13 @@ def get_ref_id():
 	except:
 		return ref_id
 
+def share(request, ref_id):
+	print ref_id
+	context = {'ref_id': ref_id}
+	template = "share.html"
+	return render(request, template, context)
 
 def home(request):
-
-
-
-	# form = EmailForm(request.POST or None)
-	# if form.is_valid():
-	# 	email=form.cleaned_data['email']
-	# 	new_join, created = Join.objects.get_or_create(email= email)
-	# 	print new_join, created
-	# 	print new_join.timestamp
-
 	form = JoinForm(request.POST or None)
 	if form.is_valid():
 		new_join = form.save(commit=False)
@@ -48,11 +43,8 @@ def home(request):
 	   		new_join_old.ref_id = get_ref_id()
 	   		new_join_old.ip_address = get_ip(request)
 	   		new_join_old.save()
-	
+	   	return HttpResponseRedirect("/%s" %(new_join_old.ref_id))
 
-
-		# new_join.ip_address = get_ip(request)
-		# new_join.save()
 
 	context = {"form": form}
 	template = "home.html"
